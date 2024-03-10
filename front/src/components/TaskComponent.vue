4b538212a3e3399f21bda2260a544009963452dd
Maks <am.xlexeev@gmail.com>
2024-03-10 11:27:55 +0300

    task and tracker components added

--- front/src/components/TaskComponent.vue
+++ front/src/components/TaskComponent.vue
@@ -1,9 +1,75 @@
-<script setup>
-</script>
-
 <template>
-
+  <div class="task">
+    <div class="task-content">
+      <h3>{{ taskText }}</h3>
+    </div>
+    <div class="timer">
+      <p>{{ formattedTime }}</p>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  props: {
+    taskText: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      timer: 0,
+      timerInterval: null,
+    };
+  },
+  mounted() {
+    this.startTimer();
+  },
+  computed: {
+    formattedTime() {
+      const hours = Math.floor(this.timer / 3600);
+      const minutes = Math.floor((this.timer % 3600) / 60);
+      const seconds = this.timer % 60;
+
+      if (hours > 0) {
+        return `${hours}:${this.padZero(minutes)}:${this.padZero(seconds)} hour${hours > 1 ? 's' : ''}`;
+      } else if (minutes > 0) {
+        return `${minutes}:${this.padZero(seconds)} min`;
+      } else {
+        return `${seconds} sec`;
+      }
+    },
+  },
+  methods: {
+    startTimer() {
+      this.timerInterval = setInterval(() => {
+        this.timer++;
+      }, 1000);
+    },
+    stopTimer() {
+      clearInterval(this.timerInterval);
+    },
+    padZero(value) {
+      return value < 10 ? `0${value}` : value;
+    },
+  },
+  beforeDestroy() {
+    this.stopTimer();
+  },
+};
+</script>
+
+
 <style scoped>
+.task {
+  display: flex;
+  justify-content: space-between;
+}
+.task-content {
+  flex: 1;
+}
+.timer {
+  margin-right: 10%;
+}
 </style>
--- front/src/components/TrackerComponent.vue
+++ front/src/components/TrackerComponent.vue
@@ -1,18 +1,48 @@
-<script setup>
-
-</script>
-
 <template>
     <div class="tracker">
-      <textarea> </textarea>
+      <textarea v-model="taskText"></textarea>
+      <img 
+        class="play-button"
+        src="../assets/play-button.png" 
+        alt="Add Task" 
+        @click="addTask" 
+      />
+      <div v-for="(task, index) in tasks" :key="index">
+        <component :is="task.component" :taskText="task.taskText" />
+      </div>
     </div>
 </template>
 
 <style scoped>
-    .tracker {
-      padding: 2%;
-      margin: 2%;
-      width: 100vh;
-      height: 20vh;
-    }
+.tracker {
+    padding: 2%;
+    margin: 2%;
+    width: 100vh;
+    height: 20vh;
+}
+.play-button {
+    padding: 1%;
+}
 </style>
+
+<script>
+import TaskComponent from '@/components/TaskComponent.vue';
+
+export default {
+  data() {
+    return {
+      tasks: [],
+      taskText: "",
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({
+        component: TaskComponent,
+        taskText: this.taskText,
+      });
+      this.taskText = "";
+    },
+  },
+};
+</script>
